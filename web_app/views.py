c46a6d008b8fa42ce4dea3f6fe39c4a79b0b38a0
pocaroline34 <39481606+pocaroline34@users.noreply.github.com>
2019-08-17 08:40:42 +0700

    update2

--- web_app/views.py
+++ web_app/views.py
@@ -4,6 +4,23 @@ from django.http import JsonResponse
 
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+#yang ditambah
+from . import utils
 
 def makanan(request):
     return render(request,'home.html')
+
+#ambek dari alzheimerai dan ubah disini
+def hasil_json(request):
+
+    data = {
+        'name': request.POST.get('name', ''),
+        'age': request.POST.get('age', ''),
+        'height': request.POST.get('height', ''),
+        'weight': request.POST.get('weight', ''),
+        'gender': request.POST.get('gender', ''),
+        'activity': request.POST.get('activity', ''),
+    }
+    utils.gerate_report(data)
+
+    return JsonResponse(data)
